de6e743b95675fbd852ac72821c026de3ad841e2
Trần Cường <trancuongitus@gmail.com>
2021-11-10 11:16:43 +0700

    Tạo màn hình Infomation
    Màn hình Infomation Activity

---
+++ app/src/main/java/com/example/photoapp/InfomationActivity.java
@@ -0,0 +1,14 @@
+package com.example.photoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfomationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infomation_activity);
+    }
+}
