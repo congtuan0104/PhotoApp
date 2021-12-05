13e455502fee387a6c5396e41831049d1b0834b4
Caden <trinhde530@gmail.com>
2021-12-05 14:41:37 +0700

    Detail More PopUp

--- app/src/main/java/com/example/photoapp/DetailPhotoActivity.java
+++ app/src/main/java/com/example/photoapp/DetailPhotoActivity.java
@@ -3,11 +3,15 @@ package com.example.photoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 public class DetailPhotoActivity extends AppCompatActivity {
     private Photo mPhoto;
     private ImageView bigImg;
+
+    ImageView detailMoreBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +22,21 @@ public class DetailPhotoActivity extends AppCompatActivity {
             return;
         }
         mPhoto = (Photo) bundle.getParcelable("object_photo");
-
         bigImg = (ImageView)findViewById(R.id.bigImg);
         bigImg.setImageURI(mPhoto.getImgUri());
 
+        detailMoreBtn = findViewById(R.id.detailMore);
+        detailMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowDetailMore();
+            }
+        });
+    }
+
+    private void ShowDetailMore(){
+        PopupMenu detailMenu = new PopupMenu(this, detailMoreBtn);
+        detailMenu.getMenuInflater().inflate(R.menu.detail_more, detailMenu.getMenu());
+        detailMenu.show();
     }
 }
